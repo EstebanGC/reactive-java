8f66f7e31f54a5a998820c09bc0ce07b289b0e1d
Johan Galeano Carvajal <johan.galeano@sofka.local>
2023-02-09 01:24:33 -0500

    Initial commit
    Learning about subscribe method

---
+++ src/main/java/com/example/demo/ReactivityApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class ReactivityApplication implements CommandLineRunner {
+
+//	private static final Logger log = LoggerFactory.getLogger(arg0);
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivityApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> names = Flux.just("Vanessa", "Jesse","Curly", "Tony")
+				.doOnNext(System.out::println);
+
+		names.subscribe();
+	}
+}
