40ecba3bf56c255e24f4544ba4574fb829d497bd
EstebanGC <johan.gal.car@gmail.com>
2023-02-09 16:06:43 -0500

    Setting thins up in a new pc

--- src/main/java/com/example/demo/ReactivityApplication.java
+++ src/main/java/com/example/demo/ReactivityApplication.java
@@ -11,13 +11,15 @@ import reactor.core.publisher.Flux;
 public class ReactivityApplication implements CommandLineRunner {
 
 //	private static final Logger log = LoggerFactory.getLogger(arg0);
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ReactivityApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> names = Flux.just("Vanessa", "Jesse","Curly", "Tony")
+		Flux<String> names = Flux.just("Vanessa", "Jesse","Curly", "Tony", "Mike")
 				.doOnNext(System.out::println);
 
 		names.subscribe();
